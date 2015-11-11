1305396740c071a0edf5ea24251f72064bb12fdc
Ramsey <rdarling42@gmail.com>
2015-11-11 13:45:56 -0500

    created controller and model for components. also, realizing that this prototype should stay a prototype and that the actual production web app, with multiple user accounts, should be built from scratch

---
+++ app/helpers/component_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Saw
+  class App
+    module ComponentHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers ComponentHelper
+  end
+end
